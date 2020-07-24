f90aac3f918d0a84648b33216f7462da095212e9
andresnsn <59550765+andresnsn@users.noreply.github.com>
2020-07-24 16:31:12 -0300

    AppGif

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:appgif/UI/gif_date.dart';
+import 'package:flutter/material.dart';
+import 'package:appgif/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
